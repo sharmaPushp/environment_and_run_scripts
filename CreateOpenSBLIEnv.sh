4541440b0eef8966f98701d579f89fe3be649d40
jpmeng <jpmeng@gmail.com>
2022-11-15 11:45:00 +0000

    Add machine type to CreateEnv.sh

--- CreateOpenSBLIEnv.sh
+++ CreateOpenSBLIEnv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ $# -eq 0 ]
+if [ $# -ne 2 ]
 then
     echo "This script will create the whole OpenSBLI environment !"
     echo "Usage: ./CreateOpenSBLIEnv.sh Dir"
     echo "Dir: the directory for the environment. Note: Dir must be a absoulte path"
+    echo "Machine type: Ubuntu ARCHER2"
     exit 1
 fi
 mkdir -p $1
@@ -12,7 +13,7 @@ wget -c https://github.com/jpmeng/utilities/archive/refs/heads/main.zip
 unzip -j main.zip
 rm main.zip
 ./InstallPython2.sh $1/Python2
-./InstallOPS.sh $1/OPS-INSTALL ARCHER2
+./InstallOPS.sh $1/OPS-INSTALL $2
 wget -c https://github.com/opensbli/opensbli/archive/refs/heads/cpc_release.zip
 unzip cpc_release.zip
 rm cpc_release.zip
